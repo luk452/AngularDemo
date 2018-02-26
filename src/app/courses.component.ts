4de5285b8215884b0946fe3ba58c0ed507df2038
Luk452 <luk452@icloud.com>
2018-02-26 17:58:12 +0100

    added bottstrap; property binding; class binding

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -6,16 +6,20 @@ import { Component } from '@angular/core';
     selector: 'courses', // <courses>
     template: `
         <h2>{{ title }}</h2>
+        <img [src]="imageUrl" />
         <ul>
             <li *ngFor="let course of courses">
                 {{ course }}
             </li>
         </ul>
+        <button class="btn btn-primary" [class.active]="isActive">Save</button>
     `
 })
 
 export class CoursesComponent {
     title = "List of courses";
+    imageUrl = "http://lorempixel.com/g/400/100";
+    isActive = true;
     courses;
 
     // dependency in constructor
