03efa25de57bf1e5356a7603965d84a3c8dc26b6
Luk452 <luk452@icloud.com>
2018-02-26 17:04:23 +0100

    Authors exercise - my implementation

---
+++ src/app/authors/authors.component.ts
@@ -0,0 +1,21 @@
+import { AuthorsService } from './../authors.service';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'authors',
+  templateUrl: './authors.component.html',
+  styleUrls: ['./authors.component.css']
+})
+export class AuthorsComponent implements OnInit {
+  // fields:
+  title = "Authors:";
+  authors;
+
+  constructor(authorsService: AuthorsService) {
+    this.authors = authorsService.getAuthors();
+  }
+
+  ngOnInit() {
+  }
+
+}
