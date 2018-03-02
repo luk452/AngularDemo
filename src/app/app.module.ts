56597c469f309e8ae68505bb9562c1e1cbbfbfe0
Luk452 <luk452@icloud.com>
2018-03-02 15:58:28 +0100

    ngFor - change detection, TrackBy for performance

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { FavoriteComponent } from './favorite/favorite.component';
 import { TitleCasePipe } from './title-case.pipe';
 import { PanelComponent } from './panel/panel.component';
 import { LikeComponent } from './like/like.component';
+import { Courses2Component } from './courses2/courses2.component';
 
 
 @NgModule({
@@ -26,7 +27,8 @@ import { LikeComponent } from './like/like.component';
     FavoriteComponent,
     TitleCasePipe,
     PanelComponent,
-    LikeComponent
+    LikeComponent,
+    Courses2Component
   ],
   imports: [
     BrowserModule,
---
+++ src/app/courses2/courses2.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'courses2',
+  templateUrl: './courses2.component.html',
+  styleUrls: ['./courses2.component.css']
+})
+export class Courses2Component {
+
+  courses;
+
+  onAdd() {
+    this.courses.push({ id: 4, name: 'new c4'});
+  }
+
+  onRemove(course) {
+    let index = this.courses.indexOf(course);
+    this.courses.splice(index, 1);
+  }
+
+  onChange(course) {
+    course.name += ' UPDATED!';
+  }
+
+  loadCourses () {
+    this.courses = [
+      { id: 1, name: 'c1'},
+      { id: 2, name: 'c2'},
+      { id: 3, name: 'c3'}
+    ];
+  }
+
+  trackCourse(index, course) {
+    return course ? course.id : undefined;
+  }
+
+}
