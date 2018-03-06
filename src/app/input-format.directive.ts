40250f7af52829be6d3bb1b676ba45d298cea4cb
Luk452 <luk452@icloud.com>
2018-03-06 14:31:58 +0100

    Custom directive for formatting input field

--- src/app/input-format.directive.ts
+++ src/app/input-format.directive.ts
@@ -1,10 +1,26 @@
-import { Directive } from '@angular/core';
+import { Directive, HostListener, ElementRef, Input } from '@angular/core';
 
 @Directive({
   selector: '[appInputFormat]'
 })
 export class InputFormatDirective {
+  // @Input ('format') format;
+  // As we have only 1 input we can simplify it:
+  @Input ('appInputFormat') format;
+  
+  constructor(private el: ElementRef) { }
 
-  constructor() { }
+  @HostListener('focus') onFocus() {
+    console.log("onFocus");
+  }
+
+  @HostListener('blur') onBlur() {
+    console.log("onBlur");
+    let value: string = this.el.nativeElement.value;
+    if (this.format == 'lowercase')
+      this.el.nativeElement.value = value.toLowerCase();
+    else  
+    this.el.nativeElement.value = value.toUpperCase();
+  }
 
 }
