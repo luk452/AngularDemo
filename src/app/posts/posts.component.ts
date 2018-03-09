ff3e3657a892fd02a6f25776f1f4460ffa1e0d21
Luk452 <luk452@icloud.com>
2018-03-09 15:50:37 +0100

    Http: POST, PUT, PATCH

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -1,22 +1,46 @@
-import { Component, OnInit } from '@angular/core';
-import { Http } from '@angular/http';
+import { Component, OnInit } from "@angular/core";
+import { Http } from "@angular/http";
 
 @Component({
-  selector: 'posts',
-  templateUrl: './posts.component.html',
-  styleUrls: ['./posts.component.css']
+  selector: "posts",
+  templateUrl: "./posts.component.html",
+  styleUrls: ["./posts.component.css"]
 })
 export class PostsComponent implements OnInit {
   posts: any[];
+  private url = "https://jsonplaceholder.typicode.com/posts";
+  // private url = 'http://local.srsoft.com:5000/api/promos';
 
-  constructor(http: Http) { 
-    http.get('https://jsonplaceholder.typicode.com/posts')
+  constructor(private http: Http) {
+    // http.get returns Observable<Response>. It means that it's async. Observable has method subscribe. Define subscription function.
+    http.get(this.url).subscribe(response => {
+      this.posts = response.json();
+    });
+  }
+
+  createPost(input: HTMLInputElement) {
+    let postObj = {
+      title: input.value
+    };
+    input.value = "";
+
+    this.http.post(this.url, JSON.stringify(postObj)).subscribe(response => {
+      postObj["id"] = response.json().id;
+      this.posts.splice(0, 0, postObj);
+      console.log(response.json());
+    });
+  }
+
+  updatePost(input) {
+    // this.http.patch - update only a few properties
+    // this.http.put - send whole object
+    
+    this.http.patch(this.url + '/' + input.id, JSON.stringify({ isRead: true }))
       .subscribe(response => {
-        this.posts = response.json();
+        console.log(response.json());
       });
+    //this.http.put(this.url, JSON.stringify(input));
   }
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
