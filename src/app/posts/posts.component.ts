f2b6e34fc560be9cf08469ee69df2c4b1df66ad9
Luk452 <luk452@icloud.com>
2018-03-12 14:54:48 +0100

    OnInit interface; http.DELETE

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -11,9 +11,12 @@ export class PostsComponent implements OnInit {
   private url = "https://jsonplaceholder.typicode.com/posts";
   // private url = 'http://local.srsoft.com:5000/api/promos';
 
-  constructor(private http: Http) {
-    // http.get returns Observable<Response>. It means that it's async. Observable has method subscribe. Define subscription function.
-    http.get(this.url).subscribe(response => {
+  constructor(private http: Http) {}
+  ngOnInit() {
+    // on of the lifecycle hooks
+    //http.get returns Observable<Response>. It means that it's async. Observable has method subscribe. Define subscription function.
+    let observableResponse = this.http.get(this.url); // :Observable<Response>
+    observableResponse.subscribe(response => {
       this.posts = response.json();
     });
   }
@@ -34,13 +37,20 @@ export class PostsComponent implements OnInit {
   updatePost(input) {
     // this.http.patch - update only a few properties
     // this.http.put - send whole object
-    
-    this.http.patch(this.url + '/' + input.id, JSON.stringify({ isRead: true }))
+
+    this.http
+      .patch(this.url + "/" + input.id, JSON.stringify({ isRead: true }))
       .subscribe(response => {
         console.log(response.json());
       });
     //this.http.put(this.url, JSON.stringify(input));
   }
 
-  ngOnInit() {}
+  deletePost(input) {
+    this.http.delete(this.url + "/" + input.id)
+      .subscribe(response => {
+        let index = this.posts.indexOf(input);
+        this.posts.splice(index, 1);
+      });
+  }
 }
