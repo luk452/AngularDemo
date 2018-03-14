54612897921b8f5c3d2b0eb37b05e8dc573dcb62
Luk452 <luk452@icloud.com>
2018-03-14 17:09:43 +0100

    Handling Errors

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -1,4 +1,4 @@
-import { PostService } from './../services/post.service';
+import { PostService } from "./../services/post.service";
 import { Component, OnInit } from "@angular/core";
 
 @Component({
@@ -14,9 +14,15 @@ export class PostsComponent implements OnInit {
     // on of the lifecycle hooks
     //http.get returns Observable<Response>. It means that it's async. Observable has method subscribe. Define subscription function.
     let observableResponse = this.service.getPosts(); // :Observable<Response>
-    observableResponse.subscribe(response => {
-      this.posts = response.json();
-    });
+    observableResponse.subscribe(
+      response => {
+        this.posts = response.json();
+      },
+      error => {
+        alert("An unexpected error occurred.");
+        console.log(error);
+      }
+    );
   }
 
   createPost(input: HTMLInputElement) {
@@ -26,25 +32,48 @@ export class PostsComponent implements OnInit {
     input.value = "";
 
     let observableResponse = this.service.createPost(postObj);
-    observableResponse.subscribe(response => {
-      postObj["id"] = response.json().id;
-      this.posts.splice(0, 0, postObj);
-      console.log(response.json());
-    });
+    observableResponse.subscribe(
+      response => {
+        postObj["id"] = response.json().id;
+        this.posts.splice(0, 0, postObj);
+        console.log(response.json());
+      },
+      (error: Response) => {
+        if (error.status === 400) {
+          // this.form.setErrors(error.json());
+        } else {
+          alert("An unexpected error occurred.");
+          console.log(error);
+        }
+      }
+    );
   }
 
   updatePost(input: HTMLInputElement) {
-    this.service.updatePost(input)
-      .subscribe(response => {
+    this.service.updatePost(input).subscribe(
+      response => {
         console.log(response.json());
-      });
+      },
+      error => {
+        alert("An unexpected error occurred.");
+        console.log(error);
+      }
+    );
   }
 
   deletePost(input: HTMLInputElement) {
-    this.service.deletePost(input.id)
-      .subscribe(response => {
+    this.service.deletePost(input.id).subscribe(
+      response => {
         let index = this.posts.indexOf(input);
         this.posts.splice(index, 1);
-      });
+      },
+      (error: Response) => {
+        if (error.status === 404) alert("This post has already been deleted.");
+        else {
+          alert("An unexpected error occurred.");
+          console.log(error);
+        }
+      }
+    );
   }
 }
